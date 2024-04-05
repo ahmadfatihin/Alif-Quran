e944b4b6092dea2c2c55abf7f1b38bd0145f35e4
Fatih <ahmdfatihin17@gmail.com>
2024-04-05 07:21:07 +0700

    feat: add geolocator dependencies and register it in injector

--- lib/core/di/register_module.dart
+++ lib/core/di/register_module.dart
@@ -1,5 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:injectable/injectable.dart';
 import 'package:logger/logger.dart';
 
@@ -8,6 +9,7 @@ import '../env/env.dart';
 //Module required to register that our service depends on
 @module
 abstract class RegisterModule {
+  Geolocator get geolocation => Geolocator();
   Connectivity get connectivity => Connectivity();
   Logger get logger => Logger();
 
