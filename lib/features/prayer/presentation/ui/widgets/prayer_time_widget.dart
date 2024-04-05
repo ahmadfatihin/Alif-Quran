719d6052cd8db9187b25655efa808f8f66759d36
Fatih <ahmdfatihin17@gmail.com>
2024-04-05 13:27:49 +0700

    feat: implement cubit to ui
    Cubit acts as a waitress to deliver and manage ui state based on usecase result

--- lib/features/prayer/presentation/ui/widgets/prayer_time_widget.dart
+++ lib/features/prayer/presentation/ui/widgets/prayer_time_widget.dart
@@ -1,19 +1,38 @@
+import 'package:alif_quran/features/prayer/presentation/cubit/prayer_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
 
-class PrayerTimeWidget extends StatelessWidget {
+@singleton
+class PrayerTimeWidget extends StatefulWidget {
   const PrayerTimeWidget({super.key});
 
   @override
+  State<PrayerTimeWidget> createState() => _PrayerTimeWidgetState();
+}
+
+class _PrayerTimeWidgetState extends State<PrayerTimeWidget> {
+  @override
+  void initState() {
+    GetIt.I<PrayerCubit>().getLocation();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'Dzuhur 11.57',
-        ),
-        Text(
-          '10 menit lagi',
-        ),
-      ],
+    return BlocProvider.value(
+      value: GetIt.I<PrayerCubit>(),
+      child: const Column(
+        children: [
+          Text(
+            'Dzuhur 11.57',
+          ),
+          Text(
+            '10 menit lagi',
+          ),
+        ],
+      ),
     );
   }
 }
