8a659852fa19d67a08f71e56294212f6e3f7976a
Fatih <ahmdfatihin17@gmail.com>
2024-04-09 19:11:21 +0700

    feat: create quran remote data source get surah

---
+++ lib/features/quran/data/datasources/quran_remote_datasources.dart
@@ -0,0 +1,26 @@
+import 'package:alif_quran/features/quran/data/models/surah_model.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../../core/core.dart';
+
+abstract class QuranRemoteDataSource {
+  Future<SurahModel> getQuran({required String id});
+}
+
+@LazySingleton(as: QuranRemoteDataSource)
+class QuranRemoteDataSourceImpl implements QuranRemoteDataSource {
+  final HttpClientService httpClientService;
+
+  QuranRemoteDataSourceImpl({required this.httpClientService});
+  @override
+  Future<SurahModel> getQuran({required String id}) async {
+    try {
+      final response = await httpClientService.get(
+        path: 'https://quran-api.santrikoding.com/api/surah/$id',
+      );
+      return SurahModel.fromJson(response.data);
+    } catch (e) {
+      throw ServerException(error: e.toString());
+    }
+  }
+}
