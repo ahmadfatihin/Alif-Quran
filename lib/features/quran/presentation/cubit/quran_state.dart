c82f6e5dbf8935908936a38f733604633142e492
Fatih <ahmdfatihin17@gmail.com>
2024-04-09 12:01:35 +0700

    feat: create quran cubit

---
+++ lib/features/quran/presentation/cubit/quran_state.dart
@@ -0,0 +1,9 @@
+part of 'quran_cubit.dart';
+
+@freezed
+class QuranState with _$QuranState {
+  const factory QuranState.initial() = _Initial;
+  const factory QuranState.loading() = _Loading;
+  const factory QuranState.success() = _Success;
+  const factory QuranState.error({required AppException error}) = _Error;
+}
