7927c1e920bed2f239767df9be09c69b82b16a42
Fatih <ahmdfatihin17@gmail.com>
2024-04-10 22:22:29 +0700

    feat: create cubit for controlling getQuran Usecase and handling errors

--- lib/features/quran/presentation/cubit/quran_cubit.dart
+++ lib/features/quran/presentation/cubit/quran_cubit.dart
@@ -1,10 +1,26 @@
 import 'package:alif_quran/core/core.dart';
+import 'package:alif_quran/features/quran/data/models/surah_model.dart';
+import 'package:alif_quran/features/quran/domain/usecases/get_quran.dart';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'quran_state.dart';
 part 'quran_cubit.freezed.dart';
 
+@singleton
 class QuranCubit extends Cubit<QuranState> {
-  QuranCubit() : super(const QuranState.initial());
+  final GetQuran getQuran;
+  QuranCubit(this.getQuran) : super(const QuranState.initial());
+
+  void getQuranData(String id) async {
+    emit(const QuranState.loading());
+    final result = await getQuran(id);
+    result.fold(
+      (error) {
+        emit(QuranState.error(error: error));
+      },
+      (result) => emit(QuranState.success(surahModel: result)),
+    );
+  }
 }
--- lib/features/quran/presentation/cubit/quran_state.dart
+++ lib/features/quran/presentation/cubit/quran_state.dart
@@ -4,6 +4,6 @@ part of 'quran_cubit.dart';
 class QuranState with _$QuranState {
   const factory QuranState.initial() = _Initial;
   const factory QuranState.loading() = _Loading;
-  const factory QuranState.success() = _Success;
+  const factory QuranState.success({required SurahModel surahModel}) = _Success;
   const factory QuranState.error({required AppException error}) = _Error;
 }
