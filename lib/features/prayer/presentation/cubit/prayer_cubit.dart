16be89f221c43d9a89192fb4c3af282aaa3f9648
rizki <rizkiadnanfutuh@gmail.com>
2024-11-01 06:33:23 +0700

    feat: create prayer cubit

--- lib/features/prayer/presentation/cubit/prayer_cubit.dart
+++ lib/features/prayer/presentation/cubit/prayer_cubit.dart
@@ -1,25 +1,52 @@
-import 'package:alif_quran/core/services/geolocation/city_detail_object.dart';
-import 'package:alif_quran/features/prayer/domain/usecases/get_location_data.dart';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import '../../../../core/core.dart';
+import '../../../../core/services/geolocation/geolocation_service.dart';
+import '../../data/models/prayer_time_model.dart';
+import '../../domain/usecases/get_prayer_time.dart';
 
 part 'prayer_state.dart';
 part 'prayer_cubit.freezed.dart';
 
 @singleton
 class PrayerCubit extends Cubit<PrayerState> {
-  late GetLocationData getLocationData;
-  PrayerCubit({
-    required this.getLocationData,
+  final GetPrayerTimes getPrayerTimes;
+  final GeolocationService geolocationService;
+  bool _isDataFetched = false;
+
+  PrayerCubit(
+    this.geolocationService, {
+    required this.getPrayerTimes,
   }) : super(const PrayerState.initial());
 
-  void getLocation() async {
+  void fetchPrayerTime() async {
+    if (_isDataFetched) return;
+
     emit(const PrayerState.loading());
-    final result = await getLocationData();
-    result.fold(
-      (error) => emit(const PrayerState.error()),
-      (result) => emit(PrayerState.success(cityDetail: result)),
-    );
+
+    try {
+      final position = await geolocationService.getCurrentPosition();
+      final cityDetail = await geolocationService.getCityDetails(position);
+      final prayerTimeResult = await getPrayerTimes(
+        cityDetail.locality,
+        cityDetail.country,
+      );
+
+      prayerTimeResult.fold(
+        (error) => emit(PrayerState.error(error: error)),
+        (prayerTime) {
+          _isDataFetched = true; // Tandai data sudah diambil
+          emit(PrayerState.success(prayerTime: prayerTime));
+        },
+      );
+    } catch (error) {
+      emit(PrayerState.error(
+          error: DefaultAppException(message: error.toString())));
+    }
+  }
+
+  void resetData() {
+    _isDataFetched = false;
   }
 }
