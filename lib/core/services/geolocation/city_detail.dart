1946188cc7b91c090177070231636189e6286b38
rizki <rizkiadnanfutuh@gmail.com>
2024-11-01 06:37:45 +0700

    feat: add required parameters

--- lib/core/services/geolocation/city_detail.dart
+++ lib/core/services/geolocation/city_detail.dart
@@ -1,13 +1,17 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'city_detail_object.freezed.dart';
-part 'city_detail_object.g.dart';
+part 'city_detail.freezed.dart';
+part 'city_detail.g.dart';
 
 @freezed
 class CityDetail with _$CityDetail {
   const factory CityDetail({
     required String subAdministrativeArea,
     required String locality,
+    required String country,
+    required String date,
+    required String month,
+    required String year,
   }) = _CityDetail;
 
   factory CityDetail.fromJson(Map<String, dynamic> json) =>
