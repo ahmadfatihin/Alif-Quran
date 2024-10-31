46c13798d6b637fc9dd48270714bd6bc94a5a6fe
rizki <rizkiadnanfutuh@gmail.com>
2024-11-01 05:52:36 +0700

    feat: add route to listquranscreen

--- lib/core/routes/routes_config.dart
+++ lib/core/routes/routes_config.dart
@@ -1,9 +1,18 @@
 import 'package:alif_quran/features/home/ui/screens/home_screen.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/listquran/presentation/ui/screens/list_quran_sccreen.dart';
+
 class RoutesConfig {
   static String home = '/';
   static GoRouter appRouter = GoRouter(initialLocation: home, routes: <GoRoute>[
-    GoRoute(path: home, builder: (context, state) => const HomeScreen()),
+    GoRoute(
+      path: home,
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/listQuran',
+      builder: (context, state) => const ListQuranScreen(),
+    ),
   ]);
 }
