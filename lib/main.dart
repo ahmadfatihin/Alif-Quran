cc0bf08549ce1599be638160ff5c2e2e0f242e8f
rizki <rizkiadnanfutuh@gmail.com>
2024-11-03 15:52:50 +0700

    feat: add multiple BlocProviders for Listquran, Quran, Location,  Prayer, and  BottomNav

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,20 @@ import 'package:get_it/get_it.dart';
 
 import 'core/routes/routes_config.dart';
 import 'features/listquran/presentation/cubit/listquran_cubit.dart';
+import 'features/navbar/presentation/cubit/bottomnav_cubit.dart';
+import 'features/navbar/presentation/ui/screens/home/presentation/cubit/location_cubit.dart';
+import 'features/prayer/presentation/cubit/prayer_cubit.dart';
+import 'features/quran/presentation/cubit/quran_cubit.dart';
 
 void main() async {
   await ScreenUtil.ensureScreenSize();
   await configureDependencies();
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -23,10 +27,23 @@ class MyApp extends StatelessWidget {
       child: MultiBlocProvider(
         providers: [
           BlocProvider(
-            create: (context) => GetIt.I<ListquranCubit>(),
+            create: (context) => GetIt.I<ListquranCubit>()..getListQuranData(),
+          ),
+          BlocProvider(
+            create: (context) => GetIt.I<QuranCubit>(),
+          ),
+          BlocProvider(
+            create: (context) => GetIt.I<LocationCubit>()..fetchLocation(),
+          ),
+          BlocProvider(
+            create: (context) => GetIt.I<PrayerCubit>()..fetchPrayerTime(),
+          ),
+          BlocProvider(
+            create: (context) => BottomNavCubit(),
           ),
         ],
         child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
           routerConfig: RoutesConfig.appRouter,
         ),
       ),
